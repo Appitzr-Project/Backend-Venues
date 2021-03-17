681b3b417c891308d7ce1169c1613325a18f447d
Vibi Primantono <vibi94@gmail.com>
2021-03-18 01:07:19 +0700

    Udate faforiteController
    favoriteStoreValidate
    favoriteDeleteValidate
    favoriteStore
    favoriteDelete

--- controller/favoriteController.ts
+++ controller/favoriteController.ts
@@ -1,7 +1,7 @@
 import { Response, NextFunction } from "express";
 import { body, validationResult, ValidationChain } from 'express-validator';
-import { userFavoritesModel, userFavorites } from "@appitzr-project/db-model";
-import { RequestAuthenticated, validateGroup } from "@base-pojokan/auth-aws-cognito";
+import { userFavoritesModel, userFavorites, userProfileModel } from "@appitzr-project/db-model";
+import { RequestAuthenticated, userDetail } from "@base-pojokan/auth-aws-cognito";
 import * as AWS from 'aws-sdk';
 import { v4 as uuidv4 } from 'uuid';
 
@@ -11,9 +11,12 @@ const ddb = new AWS.DynamoDB.DocumentClient({ endpoint: process.env.DYNAMODB_LOC
 /**
  * Venue Profile Store Validation with Express Validator
  */
- export const favoriteStoreValidate : ValidationChain[] = [
-    body('venueId').notEmpty().isString()
-  ];
+export const favoriteStoreValidate : ValidationChain[] = [
+  body('venueId').notEmpty().isString()
+];
+export const favoriteDeleteValidate : ValidationChain[] = [
+  body('id').notEmpty().isString()
+];
 
 /**
  * Index Data Function
@@ -22,14 +25,14 @@ const ddb = new AWS.DynamoDB.DocumentClient({ endpoint: process.env.DYNAMODB_LOC
  * @param res
  * @param next
  */
-export const favoriteIndex = async (
+export const favoriteStore = async (
   req: RequestAuthenticated,
   res: Response,
   next: NextFunction
 ) => {
   try {
     // validate group
-    const userDetail = await validateGroup(req, "venue");
+    const user = userDetail(req);
 
     // exapress validate input
     const errors = validationResult(req);
@@ -37,14 +40,28 @@ export const favoriteIndex = async (
       return res.status(400).json({ errors: errors.array() });
     }
 
-    // get input
-    const user : userFavorites = req.body;
+    // dynamodb parameter
+    const paramDB : AWS.DynamoDB.DocumentClient.GetItemInput = {
+      TableName: userProfileModel.TableName,
+      Key: {
+        email: user.email,
+        cognitoId: user.sub
+      },
+      AttributesToGet: ["id"]
+    }
 
-    // venue profile input with typescript definition
+    // query to database
+    const getUser = await ddb.get(paramDB).promise();
+
+    // get input
+    const favorite : userFavorites = req.body;
+
+    // venue Favorites input with typescript definition
     const userInput : userFavorites = {
       id: uuidv4(),
-      userId: userDetail?.sub,
-      venueId: user.venueId,
+      userId: getUser?.Item.id,
+      venueId: favorite.venueId,
+      isBlocked: false,
       createdAt: new Date().toISOString(),
       updatedAt: new Date().toISOString()
     }
@@ -52,7 +69,8 @@ export const favoriteIndex = async (
     // dynamodb parameter
     const paramsDB : AWS.DynamoDB.DocumentClient.PutItemInput = {
       TableName: userFavoritesModel.TableName,
-      Item: userInput
+      Item: userInput,
+      ConditionExpression: 'attribute_not_exists(venueId)'
     }
 
     // save data to database
@@ -66,6 +84,80 @@ export const favoriteIndex = async (
     });
 
   } catch (e) {
+    /**
+     * Return error kalau expression data udh ada
+     */
+     if(e?.code == 'ConditionalCheckFailedException') {
+      next(new Error('Data Already Exist.!'));
+    }
+
+    // return default error
+    next(e);
+  }
+};
+
+export const favoriteDelete = async (
+  req: RequestAuthenticated,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    // validate group
+    const user = userDetail(req);
+
+    // exapress validate input
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    // dynamodb parameter
+    const paramDB : AWS.DynamoDB.DocumentClient.GetItemInput = {
+      TableName: userProfileModel.TableName,
+      Key: {
+        email: user.email,
+        cognitoId: user.sub
+      },
+      AttributesToGet: ["id"]
+    }
+
+    // query to database
+    const getUser = await ddb.get(paramDB).promise();
+
+    // get input
+    const unfavorite : userFavorites = req.body;
+
+    // dynamodb parameter
+    const paramsDBdelete : AWS.DynamoDB.DocumentClient.Delete = {
+      TableName: userFavoritesModel.TableName,
+      Key: {
+        id: unfavorite.id,
+        userId: getUser?.Item.id
+      },
+    }
+
+    // delete data to database
+    await ddb.delete(paramsDBdelete).promise();
+
+    // return result
+    return res.status(200).json({
+      code: 200,
+      message: 'success',
+      data: {
+        id: unfavorite.id,
+        userId: getUser?.Item.id
+      }
+    });
+
+  } catch (e) {
+    /**
+     * Return error kalau expression data udh ada
+     */
+     if(e?.code == 'ConditionalCheckFailedException') {
+      next(new Error('Data Already Exist.!'));
+    }
+
+    // return default error
     next(e);
   }
 };
