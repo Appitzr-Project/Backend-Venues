46fcc6f90c6fbbf40be4e9502da2bbe46af877bb
Vibi Primantono <vibi94@gmail.com>
2021-03-18 13:24:19 +0700

    update route index add /culture

--- route/index.ts
+++ route/index.ts
@@ -1,6 +1,7 @@
 import * as express from 'express';
 import { Request, Response } from 'express';
 import { favoriteStoreValidate, favoriteDeleteValidate, favoriteStore, favoriteDelete, blockVenue } from '../controller/favoriteController';
+import { cultureGet, cultureCategoryValidate } from '../controller/cultureController';
 
 // Route Declare
 const route = express.Router();
@@ -10,6 +11,8 @@ route.post('/favorite', favoriteStoreValidate, favoriteStore);
 route.delete('/favorite', favoriteDeleteValidate, favoriteDelete);
 route.post('/favoriteblock', favoriteStoreValidate, blockVenue);
 route.delete('/favoriteblock', favoriteDeleteValidate, favoriteDelete);
+
+route.post('/culture', cultureCategoryValidate, cultureGet);
 
 // health check api
 route.get('/health-check', (req: Request, res: Response) => {
