3a0468be9e8cfc4a1964891a0af509caf41fd963
Vibi Primantono <vibi94@gmail.com>
2021-03-18 09:14:38 +0700

    update route index

--- route/index.ts
+++ route/index.ts
@@ -1,14 +1,15 @@
 import * as express from 'express';
 import { Request, Response } from 'express';
-import { favoriteStoreValidate, favoriteDeleteValidate, favoriteStore, favoriteDelete } from '../controller/favoriteController';
+import { favoriteStoreValidate, favoriteDeleteValidate, favoriteStore, favoriteDelete, blockVenue } from '../controller/favoriteController';
 
 // Route Declare
 const route = express.Router();
 
 // Route List
 route.post('/favorite', favoriteStoreValidate, favoriteStore);
-route.delete('/unfavorite', favoriteDeleteValidate, favoriteDelete);
-
+route.delete('/favorite', favoriteDeleteValidate, favoriteDelete);
+route.post('/favoriteblock', favoriteStoreValidate, blockVenue);
+route.delete('/favoriteblock', favoriteDeleteValidate, favoriteDelete);
 
 // health check api
 route.get('/health-check', (req: Request, res: Response) => {
