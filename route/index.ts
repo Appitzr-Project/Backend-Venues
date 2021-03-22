03614c5249c036cf79e64052814ca1c79071eb60
Vibi Primantono <vibi94@gmail.com>
2021-03-22 17:37:51 +0700

    update route index - change param venueId to id

--- route/index.ts
+++ route/index.ts
@@ -1,6 +1,6 @@
 import * as express from 'express';
 import { Request, Response } from 'express';
-import { favoriteUpdate, getFavoriteByVenueId, favoritesGet, favoriteStoreValidate, favoriteUpdateValidate, favoriteStore, favoriteDelete } from '../controller/favoriteController';
+import { favoriteUpdate, getFavoriteById, favoritesGet, favoriteStoreValidate, favoriteUpdateValidate, favoriteStore, favoriteDelete } from '../controller/favoriteController';
 import { cultureGet } from '../controller/cultureController';
 
 import { venuesGet, venuesGetDetails } from '../controller/venueController';
@@ -14,9 +14,9 @@ route.get('/culture', cultureGet);
 route.get('/favorites', favoritesGet);
 route.get('/:venueId', venuesGetDetails);
 route.post('/favorites', favoriteStoreValidate, favoriteStore);
-route.get('/favorites/:venueId', getFavoriteByVenueId);
-route.put('/favorites/:venueId', favoriteUpdateValidate, favoriteUpdate);
-route.delete('/favorites/:venueId', favoriteDelete);
+route.get('/favorites/:id', getFavoriteById);
+route.put('/favorites/:id', favoriteUpdateValidate, favoriteUpdate);
+route.delete('/favorites/:id', favoriteDelete);
 
 // health check api
 route.get('/health-check', (req: Request, res: Response) => {
